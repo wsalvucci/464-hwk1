855ee78c2340afe041ac2435a734690f20349d8f
William <wsalvucci@outlook.com>
2019-02-10 18:21:01 -0500

    Implemented algorithms

--- ImprovedSort.java
+++ ImprovedSort.java
@@ -1,16 +1,67 @@
+import java.util.Arrays;
+
 public class ImprovedSort {
 
-    private static void insertionSort(int[] arr) {
-        // Implement insertion sort, might need to modify parameters
+    private static void insertionSort(int[] arr, int n) {
+        for (int i=1; i<n; i++) {
+        	int x = arr[i];
+        	int j = i-1;
+        	for (j = i-1; j >=0 && arr[j] > x; j--)
+        		arr[j+1] = arr[j];
+        	arr[j+1] = x;
+        }
+    }
+    
+    private static void submerge(int[] arr, int low, int mid, int high) {
+    	int n = high-low+1;
+    	int b[] = new int[n];
+    	int left = low, right = mid+1, bIdx = 0;
+    	while (left <= mid && right <= high)
+    		b[bIdx++] = (arr[left] <= arr[right]) ? arr[left++] : arr[right++];
+    	while (left <= mid)
+    		b[bIdx++] = arr[left++];
+    	while (right <= high)
+    		b[bIdx++] = arr[right++];
+    	for (int k=0; k < n; k++)
+    		arr[low+k] = b[k];
     }
 
-    private static void merge(int[] arr) {
-        // Implement merge part here, might need to modify parameters
+    private static void merge(int[] arr, int low, int high) {
+        if (low < high) {
+        	int mid = (low+high) / 2;
+        	merge(arr, low, mid);
+        	merge(arr, mid+1, high);
+        	submerge(arr, low, mid, high);
+        }
     }
 
     private static void impSort(int[] arr) {
-        // Implement improved sort
-        // should call insertionSort() and merge() at some point in this method
+        /*
+         * If the array isn't a power of 2, split the array to have one array the power of two
+         * and run merge sort on the power of two array and insertion sort on the  smaller array.
+         * 
+         * If the power of two array is <=32, still run insertion sort
+    	*/
+    	int[] copyArr = arr;
+    	boolean power2;
+    	if ((copyArr.length != 0) && ((copyArr.length & (copyArr.length - 1)) == 0))
+    		power2 = true;
+    	else
+    		power2 = false;
+    	if (power2) {
+    		if (copyArr.length <= 32)
+    			insertionSort(copyArr, copyArr.length);
+    		else
+    			merge(copyArr, 0, copyArr.length-1);
+    	} else {
+    		System.out.println("Reducing to power of 2");
+    		int endPoint = copyArr.length;
+    		while ((endPoint != 0) && ((endPoint & (endPoint - 1)) == 0))
+    			endPoint--;
+    		merge(copyArr, 0, endPoint-1);
+    		insertionSort(copyArr, copyArr.length);
+    	}
+    	arr = copyArr;
     }
 
     private static void printArr(int[] arr) {
@@ -28,6 +79,8 @@ public class ImprovedSort {
         printArr(testArr);
 
         impSort(testArr);
+        //insertionSort(testArr, testArr.length);
+        //merge(testArr, 0, testArr.length-1);
 
         System.out.println("After sorting:");
         printArr(testArr);
